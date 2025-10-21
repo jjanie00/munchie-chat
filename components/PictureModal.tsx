45001644ba45e31ed1880a08cb719250a4c89682
jjanie00 <hmyeonge@naver.com>
2025-10-21 17:28:59 +0900

    💡 PictureModal 컴포넌트 JSDoc 주석 추가

--- components/PictureModal.tsx
+++ components/PictureModal.tsx
@@ -1,16 +1,23 @@
-// 카메라 화면 이후 열리는 모달
-
 import React, { useState } from "react";
 
 interface PictureModalProps {
   imageUrl: string;
 }
 
+/**
+ * 촬영한 사진을 표시하는 모달 컴포넌트
+ * @param {string} props.imageUrl - 촬영한 사진의 URL
+ * @returns {JSX.Element}
+ */
 export default function PictureModal({ imageUrl }: PictureModalProps) {
-  // isOpen 이 true 이면 PictureModal 을 화면에 렌더링
   return (
     <div>
       <img src={imageUrl} alt="Picture" width={100} height={100} />
     </div>
   );
 }
+
+/**
+ * 이미지 모달 화면 정중앙 위치
+ * 모달 커스텀
+ */
